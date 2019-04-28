05cc6d4160e9c0024c4b63bcf2135d25e4e7e01e
ntrllog <all123@ucsd.edu>
2019-04-28 16:01:44 -0700

    Add files via upload

--- rename.py
+++ rename.py
@@ -3,8 +3,35 @@
 
 import os
 
-for foldername, subfolder, filenames in os.walk(os.getcwd()):
-   for filename in filenames:
-      listy = filename.split(' ')
-      new_name = '.'.join(listy)
-      os.rename(filename, new_name)
+currDir = os.getcwd()
+
+print("Press Ctrl+C to exit")
+print("Enter 1 to rename files only")
+print("Enter 2 to rename folders only")
+choice = input("Press any key to rename everything: ")
+remove = input("Enter word/char to remove from file/folder name: ")
+replace = input("Enter word/char to replace: ")
+
+fileFlag = True
+folderFlag = True
+
+if choice == str(1):
+    folderFlag = False
+elif choice == str(2):
+    fileFlag = False
+
+for foldername, subfolder, filenames in os.walk(currDir):
+    if fileFlag == True:
+        for filename in filenames:
+            if filename == "rename.py" or filename == "rename.bat":
+                continue
+            name, ext = os.path.splitext(filename)
+            listy = name.split(remove)
+            newName = replace.join(listy)
+            os.rename(filename, newName + ext)
+    if folderFlag == True:
+        for folder in subfolder:
+            if remove in folder:
+                newName = folder.replace(remove, replace)
+                os.rename(folder, newName.strip())
+
